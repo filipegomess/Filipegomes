b5a6e481af03755e2d143c8022a2da517df0c2eb
filipegomess <luisfilipegomes58@gmail.com>
2024-11-13 22:03:09 -0300

    jokenpô 2

--- scripts.js
+++ scripts.js
@@ -1,105 +1,51 @@
-const convertButton = document.querySelector(".convert-button")
 
-const currencySelect = document.querySelector(".currency-select")
+const result = document.querySelector('.result')
 
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
 
+let humanScoreNumber = 0
+let machineScoreNumber = 0
 
-function convertValues() {
+const playHuman = (humanChoice) => {
 
-const inputCurrencyValue = document.querySelector(".input-currency").value
-const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
-const currencyValueConverted = document.querySelector(".currency-value")
-
-console. log(currencySelect.value)
-
-const dolarToday = 5.65
-const euroToday = 6.13
-const libraToday = 7.39
-const bitcoinToday = 432.956
-
-
-
-if(currencySelect.value == "dolar"){
-
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(inputCurrencyValue / dolarToday)
-
+     playTheGame(humanChoice, playMachine())
 
 
 }
 
-if(currencySelect.value == "euro"){
+const playMachine = () => {
 
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-        style: "currency",
-        currency: "EUR"
-    }).format(inputCurrencyValue / euroToday)
+     const choices = ['pedra', 'papel', 'tesoura']
+     const randomNumber = Math.floor(Math.random() * 3)
 
+     return choices[randomNumber]
 }
 
-if(currencySelect.value == "libra"){
+const playTheGame = (human, machine) => {
 
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
-        style: "currency",
-        currency: "GBP"
-    }).format(inputCurrencyValue / libraToday)
+ console.log('Humano: ' + human + "Maquina: " + machine)
+
+ if(human === machine){
+     
+     result.innerHTML = "Deu empate!"
+
+ } else if ( 
+
+ (human === 'papel' && machine === 'pedra') ||
+ (human === 'pedra' && machine === 'tesoura') ||
+ (human === 'tesoura' && machine === 'papel')
+
+){   humanScoreNumber++
+     humanScore.innerHTML = humanScoreNumber
+     result.innerHTML = "Você ganhou"
+
+ } else {
+
+     machineScoreNumber++
+     machineScore.innerHTML = machineScoreNumber
+     result.innerHTML = "Você perdeu para alyce!"
+ }
 
 
 }
-
-if(currencySelect.value == "bitcoin"){
-
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-BT", {
-        style: "currency",
-        currency: "BTC"
-    }).format(inputCurrencyValue / bitcoinToday)
-
-
-
-}
-currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL"
-}).format(inputCurrencyValue)
-
-}
-function changecurrency(){
-    const currencyName = document.getElementById("currency-name")
-    const currencyImage = document.querySelector(".currency-img")
-    
-    if (currencySelect.value == "dolar") {
-        currencyName.innerHTML = "Dólar americano"
-        currencyImage.src = "./imagem/Dólar.png"
-    }
-
-    if (currencySelect.value == "euro") {
-        currencyName.innerHTML = "Euro"
-        currencyImage.src = "./imagem/Euro.png"
-    }
-
-    if (currencySelect.value == "libra") {
-        currencyName.innerHTML = "Libra"
-        currencyImage.src = "./imagem/Libra.png"
-    }
-
-    if (currencySelect.value == "bitcoin") {
-        currencyName.innerHTML = "Bitcoin"
-        currencyImage.src = "./imagem/Bitcoin.png"
-    }
-
-
-
-
-
-    convertValues()
-}
-
-
-
-
-
-currencySelect.addEventListener("change", changecurrency )
-convertButton.addEventListener("click", convertValues)
-
